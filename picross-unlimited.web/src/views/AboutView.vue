22c9020d286e21590cacfd2a93e0eabd80b79147
alexa darrington <adarrington@ewu.edu>
2024-06-07 21:06:19 -0700

    revamped about page

--- picross-unlimited.web/src/views/AboutView.vue
+++ picross-unlimited.web/src/views/AboutView.vue
@@ -3,10 +3,10 @@
     <v-row class = "justify-center">
       <v-col cols="12" md="8">
         <v-card>
-          <v-card-title class="text-h5 text-center pa-4"> About Page </v-card-title>
-          <v-card-text class="text-center">
-            This Project was made my Johanne McClennan, Spencer West, and Alexa Darrington. We knew
-            how much Grant loves games so we wanted to make a fun one!
+          <v-img src="/homelogo.svg" alt="Logo"></v-img>
+          <v-card-text class="text-center text-h6">
+            This Project was made my Johanne McClennan, Spencer West, and Alexa Darrington. We know
+            how much Grant loves games so we wanted to make one. Enjoy!
           </v-card-text>
           <v-card-actions class="d-flex justify-center">
             <v-btn color="white" class="bg-primary pa-2 px-5" to="/" text="Go Home" />
