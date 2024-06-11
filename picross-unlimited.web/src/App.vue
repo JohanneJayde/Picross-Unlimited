5d89dc7a4f0da32569952e703e08f867c1cf778d
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-06-10 21:56:14 -0700

    fixed props in confirm dialog and then fixed routes in navigation drawer

--- picross-unlimited.web/src/App.vue
+++ picross-unlimited.web/src/App.vue
@@ -21,17 +21,17 @@
     </v-main>
     <v-navigation-drawer v-model="showDrawer" location="left">
       <v-list>
-        <v-list-item to="about">About</v-list-item>
-        <v-list-item to="game">Play</v-list-item>
-        <v-list-item to="player">Profile</v-list-item>
+        <v-list-item to="About">About</v-list-item>
+        <v-list-item to="Game">Play</v-list-item>
+        <v-list-item to="Player">Profile</v-list-item>
       </v-list>
     </v-navigation-drawer>
     <SignInDialog v-model="showSignInDialog" />
     <ConfirmDialog
       v-model="showConfirmDialog"
-      confirm-message="Are you sure you want to logout?"
-      confirmTitle="Log Out"
-      confirmAction="Log Out"
+      message="Are you sure you want to logout?"
+      title="Log Out"
+      action="Log Out"
       @updated="logout"
     />
   </v-app>
--- picross-unlimited.web/src/components/ConfirmDialog.vue
+++ picross-unlimited.web/src/components/ConfirmDialog.vue
@@ -1,56 +1,44 @@
 <template>
-    <v-dialog v-model="modelValue" max-width="500">
-      <v-card>
-        <v-sheet color="primary">
-          <v-card-title class="text-wrap">{{ confirmTitle }}</v-card-title>
-        </v-sheet>
-        <v-card-text class="text-body-1 ma-3 text-center">{{
-          confirmMessage
-        }}</v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            variant="tonal"
-            @click="cancel()"
-            text="Cancel"
-          />
-          <v-btn
-            color="primary"
-            variant="elevated"
-            :text="confirmAction"
-            @click="close()"
-          />
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
-  
-  <script setup lang="ts">
-  const props = withDefaults(
-    defineProps<{
-      confirmTitle: string;
-      confirmMessage: string;
-      confirmAction: string;
-    }>(),
-    {
-      confirmTitle: "Confirm",
-      confirmMessage: "Are you sure?",
-      confirmAction: "Confirm",
-    }
-  );
-  
-  const emits = defineEmits(["updated", "cancel"]);
-  
-  const modelValue = defineModel<boolean>({ default: false });
-  
-  function close() {
-    modelValue.value = false;
-    emits("updated");
+  <v-dialog v-model="modelValue" max-width="500">
+    <v-card>
+      <v-sheet color="primary">
+        <v-card-title class="text-wrap">{{ confirmTitle }}</v-card-title>
+      </v-sheet>
+      <v-card-text class="text-body-1 ma-3 text-center">{{ confirmMessage }}</v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" variant="tonal" @click="cancel()" text="Cancel" />
+        <v-btn color="primary" variant="elevated" :text="confirmAction" @click="close()" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    title: string
+    messsage: string
+    action: string
+  }>(),
+  {
+    title: 'Confirm',
+    messsage: 'Are you sure?',
+    action: 'Confirm'
   }
-  
-  function cancel() {
-    modelValue.value = false;
-    emits("cancel");
-  }
-  </script>
+)
+
+const emits = defineEmits(['updated', 'cancel'])
+
+const modelValue = defineModel<boolean>({ default: false })
+
+function close() {
+  modelValue.value = false
+  emits('updated')
+}
+
+function cancel() {
+  modelValue.value = false
+  emits('cancel')
+}
+</script>
